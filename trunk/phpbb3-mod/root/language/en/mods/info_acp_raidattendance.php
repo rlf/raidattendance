c5f850908abe9efe54a64b20dd0bb3499dcb7850
rasmus.lock.larsen <rasmus.lock.larsen@f23c67f0-bfc9-11de-ad03-79fce359684b>
2009-11-20 01:28:29 +0000

    v0.0.1 Now a list of raiders and officers can be shown based on wowarmory. No database tables are created. Install scripts have also been added

--- trunk/phpbb3-mod/root/language/en/mods/info_acp_raidattendance.php
+++ trunk/phpbb3-mod/root/language/en/mods/info_acp_raidattendance.php
@@ -17,8 +17,21 @@ if (empty($lang) || !is_array($lang)) {
 // equally where a string contains only two placeholders which are used to wrap text
 // in a url you again do not need to specify an order e.g., 'Click %sHERE%s' is fine
 $lang = array_merge($lang, array(
+	// 
+	// Installation
+	//
+		'INSTALL_RAIDATTENDANCE' => 'Install Raid Attendance',
+		'INSTALL_RAIDATTENDANCE_CONFIRM' => 'Install Raid Attendance?',
+		'UPDATE_RAIDATTENDANCE' => 'Update Raid Attendance',
+		'UPDATE_RAIDATTENDANCE_CONFIRM' => 'Update Raid Attendance?',
+		'UNINSTALL_RAIDATTENDANCE' => 'Uninstall Raid Attendance',
+		'UNINSTALL_RAIDATTENDANCE_CONFIRM' => 'Uninstall Raid Attendance?',
+		'ACP_CAT_RAIDATTENDANCE'	=> 'Raid Attendance',
+	//
+	// ACP - Configuration
+	//
 		'ACP_RAIDATTENDANCE'	=> 'Raid Attendance',
-		'ACP_RAIDATTENDANCE_SETTINGS'	=> 'Raid Attendance Configuration',
+		'ACP_RAIDATTENDANCE_SETTINGS'	=> 'Configuration',
 		'ACP_RAIDATTENDANCE_SETTINGS_EXPLAIN'	=> 'MOD Configuration of the Hippie Eradicator',
 		'GUILD_SETTINGS'			=> 'Guild Configuration',
 		'GUILD_NAME'				=> 'Guild Name',
@@ -37,6 +50,29 @@ $lang = array_merge($lang, array(
 		'RAID_NIGHT_FRI'			=> 'Friday',
 		'RAID_NIGHT_SAT'			=> 'Saturday',
 		'RAID_NIGHT_SUN'			=> 'Sunday',
+		'acl_a_raidattendance'		=> array('lang' => 'Can configure Raid Attendance', 'cat' => 'misc'),
+	//
+	// ACP - Sync
+	//
+		'ACP_RAIDATTENDANCE_SYNC'	=> 'Synchronise with wowarmory',
+		'ACP_RAIDATTENDANCE_SYNC_EXPLAIN' => 'Synchronises the list of raiders with wow-armory.',
+		'RAIDERS'					=> 'Raiders',
+		'LEVEL'						=> 'Level',
+		'RANK'						=> 'Rank',
+		'CLASS'						=> 'Class',
+
+		'CLASS_1'					=> 'Warrior',
+		'CLASS_2'					=> 'Paladin',
+		'CLASS_3'					=> 'Hunter',
+		'CLASS_4'					=> 'Rogue',
+		'CLASS_5'					=> 'Priest',
+		'CLASS_6'					=> 'Death Knight',
+		'CLASS_7'					=> 'Shaman',
+		'CLASS_8'					=> 'Mage',
+		'CLASS_9'					=> 'Warlock',
+		'CLASS_11'					=> 'Death Knight',
+		'RANK_1'					=> 'Officer',
+		'RANK_3'					=> 'Raider',
 	)
 );
 ?>
