f27a9e6513479293fa77945fb962d83a40e89bec
rasmus.lock.larsen <rasmus.lock.larsen@f23c67f0-bfc9-11de-ad03-79fce359684b>
2009-12-06 18:58:16 +0000

    First official release of the MOD

---
+++ tags/phpbb3-mod-v1.0.0/root/install_raidattendance.php
@@ -0,0 +1,231 @@
+<?php
+
+/**
+*
+* @author Rapal
+* @package raidattendance
+* @copyright (c) 2008 TA
+* @license http://opensource.org/licenses/gpl-license.php GNU Public License
+*
+*/
+
+/**
+* @ignore
+*/
+define('UMIL_AUTO', true);
+define('IN_PHPBB', true);
+
+$phpbb_root_path = (defined('PHPBB_ROOT_PATH')) ? PHPBB_ROOT_PATH : './';
+$phpEx = substr(strrchr(__FILE__, '.'), 1);
+$install_root_path = './';
+
+include($phpbb_root_path . 'common.' . $phpEx);
+
+$user->session_begin();
+$auth->acl($user->data);
+$user->setup();
+
+if (!file_exists($install_root_path . 'umil/umil_auto.' . $phpEx))
+{
+        trigger_error('Please download the latest UMIL (Unified MOD Install Library) from: <a href="http://www.phpbb.com/mods/umil/">phpBB.com/mods/umil</a>', E_USER_ERROR);
+}
+
+ 
+// The name of the mod to be displayed during installation.
+$mod_name = 'Raid Attendance';
+
+/*
+* The name of the config variable which will hold the currently installed version
+* UMIL will handle checking, setting, and updating the version itself.
+*/
+
+$version_config_name = 'raidattendance_version';
+
+/*
+* The language file which will be included when installing
+* Language entries that should exist in the language file for UMIL (replace $mod_name with the mod's name you set to $mod_name above)
+* $mod_name
+* 'INSTALL_' . $mod_name
+* 'INSTALL_' . $mod_name . '_CONFIRM'
+* 'UPDATE_' . $mod_name
+* 'UPDATE_' . $mod_name . '_CONFIRM'
+* 'UNINSTALL_' . $mod_name
+* 'UNINSTALL_' . $mod_name . '_CONFIRM'
+*/
+
+$language_file = array('mods/info_acp_raidattendance', 'mods/mod_raidattendance');
+
+/*
+* Options to display to the user (this is purely optional, if you do not need the options you do not have to set up this variable at all)
+* Uses the acp_board style of outputting information, with some extras (such as the 'default' and 'select_user' options)
+*/
+
+$options = array(
+	'legend1'		=> 'GUILD_SETTINGS',
+	'guild'			=> array('lang' => 'GUILD_NAME',			'validate' => 'string',	'type' => 'text:40:255', 'explain' => false, 'default' => 'The Awakening'),
+	'realm'			=> array('lang' => 'REALM_NAME',			'validate' => 'string',	'type' => 'text:40:255', 'explain' => false, 'default' => 'Bloodhoof'),
+	'armory_link'	=> array('lang' => 'ARMORY_LINK',			'validate' => 'string',	'type' => 'text:40:255', 'explain' => true, 'default' => 'http://eu.wowarmory.com'),
+
+	'legend2'		=> 'FORUM_SETTINGS',
+	'forum_name'	=> array('lang' => 'FORUM_NAME',			'validate' => 'string',	'type' => 'text:40:255', 'explain' => true, 'default' => 'Raid Availability'),
+);
+
+ 
+/*
+* Optionally we may specify our own logo image to show in the upper corner instead of the default logo.
+* $phpbb_root_path will get prepended to the path specified
+* Image height should be 50px to prevent cut-off or stretching.
+*/
+
+//$logo_img = 'styles/prosilver/imageset/site_logo.gif';
+
+ 
+/*
+* The array of versions and actions within each.
+* You do not need to order it a specific way (it will be sorted automatically), however, you must enter every version, even if no actions are done for it.
+*
+* You must use correct version numbering.  Unless you know exactly what you can use, only use X.X.X (replacing X with an integer).
+* The version numbering must otherwise be compatible with the version_compare function - http://php.net/manual/en/function.version-compare.php
+*/
+
+$versions = array(
+	// Version 0.0.1 - Initial version
+	'1.0.0'	=> array(
+		// Lets add a config setting named test_enable and set it to true
+		'config_add' => array(
+			// v0.0.1
+			array('raidattendance_enable', true),
+			array('raidattendance_guild_name', request_var('guild', 'no guild given')),
+			array('raidattendance_realm_name', request_var('realm', 'no realm given')),
+			array('raidattendance_armory_link', request_var('armory_link', 'no armory')),
+			array('raidattendance_forum_name', request_var('forum_name', 'no forum')),
+			array('raidattendance_raid_night_mon', request_var('raid_night_mon', false)),
+			array('raidattendance_raid_night_tue', request_var('raid_night_tue', false)),
+			array('raidattendance_raid_night_wed', request_var('raid_night_wed', false)),
+			array('raidattendance_raid_night_thu', request_var('raid_night_thu', false)),
+			array('raidattendance_raid_night_fri', request_var('raid_night_fri', false)),
+			array('raidattendance_raid_night_sat', request_var('raid_night_sat', false)),
+			array('raidattendance_raid_night_sun', request_var('raid_night_sun', false)),
+			// v0.0.2
+			array('raidattendance_min_level', request_var('min_level', 80)),
+			array('raidattendance_raider_rank0', request_var('rank0', false)),
+			array('raidattendance_raider_rank1', request_var('rank1', true)),
+			array('raidattendance_raider_rank2', request_var('rank2', false)),
+			array('raidattendance_raider_rank3', request_var('rank3', true)),
+			array('raidattendance_raider_rank4', request_var('rank4', false)),
+			array('raidattendance_raider_rank5', request_var('rank5', false)),
+			array('raidattendance_raider_rank6', request_var('rank6', true)),
+			array('raidattendance_raider_rank7', request_var('rank7', false)),
+			array('raidattendance_raider_rank8', request_var('rank8', false)),
+			array('raidattendance_raider_rank9', request_var('rank9', false)),
+			// v0.0.4
+			array('raidattendance_raider_rank0_name', request_var('rank0_name', 'Guild Leader')),
+			array('raidattendance_raider_rank1_name', request_var('rank1_name', 'Rank 1')),
+			array('raidattendance_raider_rank2_name', request_var('rank2_name', 'Rank 2')),
+			array('raidattendance_raider_rank3_name', request_var('rank3_name', 'Rank 3')),
+			array('raidattendance_raider_rank4_name', request_var('rank4_name', 'Rank 4')),
+			array('raidattendance_raider_rank5_name', request_var('rank5_name', 'Rank 5')),
+			array('raidattendance_raider_rank6_name', request_var('rank6_name', 'Rank 6')),
+			array('raidattendance_raider_rank7_name', request_var('rank7_name', 'Rank 7')),
+			array('raidattendance_raider_rank8_name', request_var('rank8_name', 'Rank 8')),
+			array('raidattendance_raider_rank9_name', request_var('rank9_name', 'Rank 9')),
+		),
+
+		// Now to add some permission settings
+
+		'permission_add' => array(
+			array('a_raidattendance', true),
+			array('m_raidattendance', true),
+			array('u_raidattendance', true),
+		),
+
+
+		// How about we give some default permissions then as well?
+		'permission_set' => array(
+			// Global Role permissions
+			array('ROLE_ADMIN_FULL', 		'a_raidattendance'),
+			array('ROLE_ADMIN_STANDARD', 	'a_raidattendance'),
+			array('ROLE_ADMIN_FULL', 		'm_raidattendance'),
+			array('ROLE_MOD_FULL', 			'm_raidattendance'),
+			array('ROLE_MOD_STANDARD', 		'm_raidattendance'),
+
+			array('ROLE_FORUM_FULL', 		'u_raidattendance'),
+		),
+
+		// Alright, now lets add some modules to the ACP
+		'module_add' => array(
+			// Add a main category
+			array('acp', 'ACP_CAT_FORUMS', 'ACP_CAT_RAIDATTENDANCE'),
+
+			array('acp', 'ACP_CAT_RAIDATTENDANCE', 
+				array(
+					'module_basename'        => 'raidattendance',
+					'module_langname'        => 'ACP_RAIDATTENDANCE_SETTINGS',
+					'module_mode'                => 'settings',
+					'module_auth'                => 'acl_a_raidattendance',
+				), 
+			),
+
+			array('acp', 'ACP_CAT_RAIDATTENDANCE', 
+				array(
+					'module_basename'        => 'raidattendance',
+					'module_langname'        => 'ACP_RAIDATTENDANCE_SYNC',
+					'module_mode'                => 'sync',
+					'module_auth'                => 'acl_a_raidattendance',
+				), 
+			),
+		),
+		'table_add' => array(
+			array('phpbb_raidattendance_history', array(
+				'COLUMNS'		=> array(
+					'id'			=> array('UINT', NULL, 'auto_increment'),
+					'user_id'		=> array('UINT', 0),
+					'raider_id'		=> array('UINT', 0),
+					'time'			=> array('TIMESTAMP', 0),
+					'action'		=> array('VCHAR:255', ''),
+				),
+				'PRIMARY_KEY'		=> 'id',
+				'KEYS'				=> array(
+					'user_id'		=> array('INDEX', 'user_id'),
+					'raider_id'		=> array('INDEX', 'raider_id'),
+					),
+				),
+			),
+			array('phpbb_raidattendance_raiders', array(
+				'COLUMNS'		=> array(
+					'id'			=> array('UINT', NULL, 'auto_increment'),
+					'user_id'		=> array('UINT', NULL),
+					'name'			=> array('VCHAR_UNI:255', ''),
+					'class'			=> array('TINT:4', 0),
+					'rank'			=> array('TINT:4', 0),
+					'level'			=> array('TINT:4', 0),
+					'synced'		=> array('TIMESTAMP', NULL),
+					'created'		=> array('TIMESTAMP', 0),
+					'edited'		=> array('TIMESTAMP', 0),
+				),
+				'PRIMARY_KEY'		=> 'id',
+				'KEYS'				=> array(
+					'name'			=> array('UNIQUE', 'name'),
+				),
+			)),
+			array('phpbb_raidattendance', array(
+				'COLUMNS'		=> array(
+					'id'		=> array('UINT', NULL, 'auto_increment'),
+					'raider_id'	=> array('UINT', 0),
+					'night'		=> array('VCHAR:10', ''),
+					'status'	=> array('TINT:4', 0),
+					'time'		=> array('TIMESTAMP', 0),
+				),
+				'PRIMARY_KEY'	=> 'id',
+				'KEYS'			=> array(
+					'rid_night'	=> array('UNIQUE', array('raider_id', 'night')),
+					'night'		=> array('INDEX', 'night'),
+				),
+			)),
+		),
+	), // end of 1.0.0
+);
+ 
+// Include the UMIF Auto file and everything else will be handled automatically.
+include($install_root_path . 'umil/umil_auto.' . $phpEx);
+?> 
---
+++ tags/phpbb3-mod-v1.0.0/root/view_raidattendance.php
@@ -0,0 +1,161 @@
+<?php
+
+/**
+*
+* @author Rapal
+* @package raidattendance
+* @copyright (c) 2008 TA
+* @license http://opensource.org/licenses/gpl-license.php GNU Public License
+*
+*/
+
+global $forum_id, $phpbb_root_path, $phpEx, $template;
+
+include($phpbb_root_path . 'includes/functions_raidattendance.' . $phpEx);
+
+if (is_raidattendance_forum($forum_id)) 
+{
+	global $user, $auth;
+	$user->add_lang(array('mods/mod_raidattendance', 'mods/info_acp_raidattendance'));
+	$success = array();
+	$error = array();
+	$tstamp = isset($_POST['tstamp']) && $_POST['tstamp'] > 0 ? $_POST['tstamp'] : time();
+	$today = strftime('%Y%m%d', time());
+	$raids = get_raiding_days($tstamp);
+	$raider_db = new raider_db();
+	$raiders = array();
+	$raider_db->get_raider_list($raiders);
+	if (isset($_POST['u_action']))
+	{
+		handle_action($raiders);
+	}
+	$day_names = get_raiding_day_names($raids);
+	$attendance = get_attendance($raids);
+	$static_attendance = get_static_attendance($raids);
+	add_static_attendance($raids, $attendance, $static_attendance);
+
+	$rowno = 0;
+	$names = array_keys($raiders);
+	foreach ($raids as $raid)
+	{
+		$tm = strptime($raid, '%Y%m%d');
+		$time = tm2time($tm);
+		$template->assign_block_vars('raid_days', array(
+			'DATE'			=> sprintf($user->lang['DAY_MONTH'], post_num(strftime('%e', $time)), strftime('%B', $time)),
+			'DAY'			=> strftime('%A', $time),
+		));
+	}
+	$statusses = array(1=>'on', 2=>'off', 3=>'noshow', 0=>'future', -1=>'past', -2=>'unset');
+	foreach ($raiders as $name => $raider) 
+	{
+		$template->assign_block_vars('raiders', array(
+			'ROWNO'				=> $rowno+1,
+			'ID'				=> $raider->id,
+			'NAME'				=> $raider->name,
+			'RANK'				=> $raider->get_rank_name(),
+			'LEVEL'				=> $raider->level,
+			'CLASS'				=> $user->lang['CLASS_' . $raider->class],
+			'USER'				=> $raider->user_id,
+			'STATUS'			=> $raider->get_status(),
+			'ROW_CLASS'			=> $rowno % 2 == 0 ? 'even' : 'uneven',
+			'CHECKED'			=> $raider->is_checked() ? ' checked' : '',
+			'CSS_CLASS'			=> 'class_' . $raider->class,
+		));
+		foreach ($day_names as $day)
+		{
+			$status = $static_attendance[$raider->name][$day];
+			$template->assign_block_vars('raiders.days', array(
+				'DAY'			=> isset($user->lang['DAY_' . $day]) ? $user->lang['DAY_' . $day] : $day,
+				'DAY_KEY'		=> $day,
+				'STATUS'		=> $statusses[$status ? $status : -2],
+				));
+		}
+		foreach ($raids as $raid)
+		{
+			$future = $raid >= $today;
+			$status = $attendance[$raider->name][$raid];
+			$day_name = get_raiding_day_name($raid);
+			$is_static = $static_attendance[$raider->name][$day_name] == 2;
+			$template->assign_block_vars('raiders.raids', array(
+				'RAID'			=> $raid,
+				'STATUS'		=> $statusses[$status ? $status : ($future ? 0 : -1)],
+				'S_FUTURE'		=> $future ? '1' : '0',
+				'S_EDITABLE'	=> ($user->data['user_id'] == $raider->user_id or ($raider->user_id == 0 and $user->data['username'] == $raider->name)) and $future ? true : false,
+				'S_STATIC'		=> $is_static ? 1 : 0,
+				));
+		}
+		$rowno++;
+	}
+	$date_array = getdate($tstamp);
+	$last_week = mktime(0, 0, 0, $date_array['mon'], $date_array['mday']-7, $date_array['year']);
+	$next_week = mktime(0, 0, 0, $date_array['mon'], $date_array['mday']+7, $date_array['year']);
+	$template->assign_vars(array(
+		'RAIDATTENDANCE_TITLE' 	=> 'Correct forum',
+		'S_RAIDATTENDANCE'		=> true,
+		'S_SUCCESS'				=> sizeof($success) ? true : false,
+		'SUCCESS_MSG'			=> implode('<br/>', $success),
+		'S_ERROR'				=> sizeof($error) ? true : false,
+		'ERROR_MSG'				=> implode('<br/>', $error),
+		'S_MODERATOR'			=> $auth->acl_get('m_'),
+		'TSTAMP_NEXT'			=> $next_week,
+		'TSTAMP_PREV'			=> $last_week,
+		));
+}
+function handle_action($raiders)
+{
+	global $success, $user, $error;
+	if (!isset($_POST['u_action']) or !isset($_POST['rid']) or !isset($_POST['raid']))
+	{
+		return;
+	}
+	// TODO: Additional checking
+	$raider = get_raider_with_id($raiders, $_POST['rid']);
+	$raid = $_POST['raid'];
+	$action = $_POST['u_action'];
+	if ($raider and $raid and $action) 
+	{
+		$day = $raid;
+		if (strlen($raid) == 8)
+		{
+			$tm = strptime($raid, '%Y%m%d');
+			$time = tm2time($tm);
+			$day = sprintf($user->lang['DAY_MONTH'], post_num(strftime('%e', $time)), strftime('%B', $time));
+		}
+		$username = $user->data['username'];
+
+		if ($action == '+')
+		{
+			$raider->signon($raid);
+		}
+		else if ($action == '-')
+		{
+			$raider->signoff($raid);
+		}
+		else if ($action == 'x')
+		{
+			$raider->clear_attendance($raid);
+		}
+		else if ($action == '!')
+		{
+			$raider->noshow($raid);
+		}
+
+		$lang_array = array(
+			'+' => 'STATUS_CHANGE_ON', 
+			'-' => 'STATUS_CHANGE_OFF', 
+			'x' => 'STATUS_CHANGE_CLEAR', 
+			'!' => 'STATUS_CHANGE_NOSHOW', 
+		);
+		$lang_key = $lang_array[$action];
+		if ($username && $raider->name && $day && $user->lang[$lang_key]) 
+		{
+			$success[] = sprintf($user->lang[$lang_key], $username, $raider->name, $day);
+		}
+		else 
+		{
+			$error[] = "Error! $username, $raider->name, $day, $lang_key, " . $user->lang[$lang_key];
+		}
+
+	}
+}
+?>
