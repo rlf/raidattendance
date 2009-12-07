80076d49431a42341823353144caf9b881f06cd1
rasmus.lock.larsen <rasmus.lock.larsen@f23c67f0-bfc9-11de-ad03-79fce359684b>
2009-12-07 21:10:00 +0000

    MODX Compliant

---
+++ tags/phpbb3-mod-v1.0.2/root/includes/acp/info/acp_raidattendance.php
@@ -0,0 +1,38 @@
+<?php
+/**
+* @ignore
+*/
+if (!defined('IN_PHPBB'))
+{
+	exit;
+}  
+class acp_raidattendance_info {
+	function module()
+	{
+		return array(
+			'filename'	=> 'acp_raidattendance',
+			'title'		=> 'ACP_RAIDATTENDANCE',
+			'version'	=> '0.0.2',
+			'cat'		=> 'ACP_BOARD_CONFIGURATION',
+			'modes'		=> array(
+			    'settings' => array(
+					'title'	=> 'ACP_RAIDATTENDANCE_SETTINGS',
+					'auth'	=> 'acl_a_raidattendance',
+					'cat'	=> array('ACP_CAT_RAIDATTENDANCE')
+				),
+			    'sync' => array(
+					'title'	=> 'ACP_RAIDATTENDANCE_SYNC',
+					'auth'	=> 'acl_a_raidattendance',
+					'cat'	=> array('ACP_CAT_RAIDATTENDANCE')
+				),
+			)
+		);
+	}
+	function install()
+	{
+	}
+	function uninstall()
+	{
+	}
+}
+?>
