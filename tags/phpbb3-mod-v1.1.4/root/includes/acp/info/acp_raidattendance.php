d81e8721d06a284338f05eb3378a5fb2b06afd16
rasmus.lock.larsen <rasmus.lock.larsen@f23c67f0-bfc9-11de-ad03-79fce359684b>
2010-04-06 20:58:58 +0000

    Sorting and coloring

---
+++ tags/phpbb3-mod-v1.1.4/root/includes/acp/info/acp_raidattendance.php
@@ -0,0 +1,44 @@
+<?php
+/**
+* @ignore
+*/
+if (!defined('IN_PHPBB'))
+{
+	exit;
+}  
+class acp_raidattendance_info 
+{
+	function module()
+	{
+		return array(
+			'filename'	=> 'acp_raidattendance',
+			'title'		=> 'ACP_RAIDATTENDANCE',
+			'version'	=> '1.1.0',
+			'cat'		=> 'ACP_BOARD_CONFIGURATION',
+			'modes'		=> array(
+			    'settings' => array(
+					'title'	=> 'ACP_RAIDATTENDANCE_SETTINGS',
+					'auth'	=> 'acl_a_raidattendance',
+					'cat'	=> array('ACP_CAT_RAIDATTENDANCE'),
+				),
+			    'sync' => array(
+					'title'	=> 'ACP_RAIDATTENDANCE_SYNC',
+					'auth'	=> 'acl_a_raidattendance',
+					'cat'	=> array('ACP_CAT_RAIDATTENDANCE'),
+				),
+				'wws' => array(
+					'title' => 'ACP_RAIDATTENDANCE_WWS',
+					'auth'	=> 'acl_a_raidattendance',
+					'cat'	=> array('ACP_CAT_RAIDATTENDANCE'),
+				),
+			)
+		);
+	}
+	function install()
+	{
+	}
+	function uninstall()
+	{
+	}
+}
+?>
